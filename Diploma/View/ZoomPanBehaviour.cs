96829024870c34d79bf30bbf5e1a7304320b61b0
Valery Noskov <ven@ari-on.ru>
2026-05-05 09:07:31 +0300

    fixed measurement taking.

--- Diploma/View/ZoomPanBehaviour.cs
+++ Diploma/View/ZoomPanBehaviour.cs
@@ -12,18 +12,26 @@ public class ZoomPanBehavior : Behavior<FrameworkElement>
         => AssociatedObject.DataContext as ImageViewerViewModel;
 
     private ScrollViewer _scroll;
+    private Image _image;
     private bool _isDragging;
     private Point? _mouseDownPoint;
-    private Point? _last;
+    private Point? _lastScrolling;
+    private Point? _lastImage;
 
     protected override void OnAttached()
     {
-        AssociatedObject.Loaded += (_, _) => { _scroll = FindScrollViewer(AssociatedObject.Parent); };
+        AssociatedObject.Loaded += OnAssociatedObjectOnLoaded;
 
         AssociatedObject.MouseWheel += OnWheel;
         AssociatedObject.MouseLeftButtonDown += OnDown;
         AssociatedObject.MouseLeftButtonUp += OnUp;
         AssociatedObject.MouseMove += OnMove;
+    }
+
+    private void OnAssociatedObjectOnLoaded(object o, RoutedEventArgs routedEventArgs)
+    {
+        _scroll = (ScrollViewer)FindElement<ScrollViewer>(AssociatedObject.Parent);
+        _image = (Image)FindElement<Image>(AssociatedObject.Parent);
     }
 
     private void OnWheel(object sender, MouseWheelEventArgs e)
@@ -51,29 +59,32 @@ public class ZoomPanBehavior : Behavior<FrameworkElement>
 
     private void OnDown(object sender, MouseButtonEventArgs e)
     {
-        _last = e.GetPosition(_scroll);
+        _lastScrolling = e.GetPosition(_scroll);
+        _lastImage = e.GetPosition(_image);
         _mouseDownPoint = e.GetPosition(_scroll);
         AssociatedObject.CaptureMouse();
     }
 
     private void OnUp(object sender, MouseButtonEventArgs e)
     {
-        if (!_isDragging && _last is not null)
-            ViewModel.TakeMeasurementAsyncCommand.Execute(new PixelPoint((int)_last.Value.X, (int)_last.Value.Y));
+        if (!_isDragging && _lastImage is not null)
+            ViewModel.TakeMeasurementAsyncCommand.Execute(new PixelPoint((int)_lastImage.Value.X, 
+                (int)_lastImage.Value.Y));
 
         _isDragging = false;
         _mouseDownPoint = null;
-        _last = null;
+        _lastScrolling = null;
+        _lastImage = null;
 
         AssociatedObject.ReleaseMouseCapture();
     }
 
     private void OnMove(object sender, MouseEventArgs e)
     {
-        if (_last == null || _scroll == null) return;
+        if (_lastScrolling == null || _scroll == null) return;
 
         var current = e.GetPosition(_scroll);
-        var delta = current - _last.Value;
+        var delta = current - _lastScrolling.Value;
 
         if ((current - _mouseDownPoint!).Value.Length > 5)
             _isDragging = true;
@@ -81,17 +92,18 @@ public class ZoomPanBehavior : Behavior<FrameworkElement>
         _scroll.ScrollToHorizontalOffset(_scroll.HorizontalOffset - delta.X);
         _scroll.ScrollToVerticalOffset(_scroll.VerticalOffset - delta.Y);
 
-        _last = current;
+        _lastScrolling = current;
+        _lastImage = e.GetPosition(_image);
     }
 
-    private ScrollViewer FindScrollViewer(DependencyObject d)
+    private object FindElement<T>(DependencyObject d)
     {
-        if (d is ScrollViewer sv) return sv;
+        if (d is T sv) return sv;
 
         for (int i = 0; i < VisualTreeHelper.GetChildrenCount(d); i++)
         {
             var child = VisualTreeHelper.GetChild(d, i);
-            var result = FindScrollViewer(child);
+            var result = FindElement<T>(child);
             if (result != null) return result;
         }
 
